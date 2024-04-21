311ed436d1959067af981fd80a74b883c87ee68a
janezd <janez.demsar@fri.uni-lj.si>
2024-04-21 18:58:57 +0200

    Fix test discovery

--- setup.py
+++ setup.py
@@ -8,10 +8,15 @@ INSTALL_REQUIRES = (
     'orange3-network'
 ),
 
+EXTRAS_REQUIRE = {
+    'doc': ['sphinx', 'recommonmark', 'sphinx_rtd_theme'],
+    'test': ['coverage'],
+}
+
 LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.pypi')).read()
 
 ENTRY_POINTS = {
-        'orange3.addon': ('educational = orangecontrib.pumice', ),
+        'orange3.addon': ('pumice = orangecontrib.pumice', ),
         "orange.widgets": ("Pumice = orangecontrib.pumice.widgets", ),
         "orange.canvas.help": (
             'html-index = orangecontrib.prototypes.widgets:WIDGET_HELP_PATH'),
@@ -20,6 +25,7 @@ ENTRY_POINTS = {
 setup(
     name="Orange3-Pumice",
     description="Educational widgets for project Pumice",
+    license="BSD",
     version=VERSION,
     author='Bioinformatics Laboratory, FRI UL',
     author_email='contact@orange.biolab.si',
@@ -33,6 +39,7 @@ setup(
     package_data={"orangecontrib.pumice.widgets": ["icons/*.svg"]},
     entry_points=ENTRY_POINTS,
     install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
     namespace_packages=['orangecontrib'],
     include_package_data=True,
     test_suite="orangecontrib.prototypes.tests.suite"
